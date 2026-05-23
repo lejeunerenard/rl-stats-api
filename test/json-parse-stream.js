c266690add7f1d82207143053bed7fc1ac3a6723
Sean Zellmer <sean@lejeunerenard.com>
2026-05-23 12:42:47 -0500

    feat: Add streaming JSON parser module
    The parser can parse partial json strings once the object or array is
    complete. This is required for parsing messages from Rocket League as it
    can include partial strings per emitted chunk.

---
+++ test/json-parse-stream.js
@@ -0,0 +1,37 @@
+const test = require('brittle')
+const parseJSONStream = require('../lib/json-parse-stream.js')
+const { join } = require('path')
+const { createReadStream } = require('fs')
+const { Readable } = require('stream')
+
+test('parseJSONStream - parses json 100bytes at a time', async (t) => {
+  const fixturePath = join(__dirname, './fixtures/update-state-simple.json')
+  const fileStream = createReadStream(fixturePath, { highWaterMark: 100 })
+
+  const stream = fileStream.pipe(new parseJSONStream())
+  const result = await reduce(stream)
+
+  const json = require(fixturePath)
+  t.alike(result, [json], 'parses json emitted per line')
+})
+
+test('parseJSONStream - parse json even when emitted with start of next', async (t) => {
+  const input = new Readable()
+
+  input.push('{ "foo": ')
+  input.push('"bar" }{ "biz": "baz" }')
+  input.push(null)
+
+  const stream = input.pipe(new parseJSONStream())
+  const result = await reduce(stream)
+
+  t.alike(result, [{ foo: 'bar' }, { biz: 'baz' }], 'returns two objs')
+})
+
+const reduce = async (stream) => {
+  const arr = []
+  for await (const data of stream) {
+    arr.push(data)
+  }
+  return arr
+}
