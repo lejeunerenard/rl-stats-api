4f26936faf7f357374ca0dca314140ee0052b0db
Sean Zellmer <sean@lejeunerenard.com>
2026-05-23 11:20:52 -0500

    feat: Add JSON parsing & basic debug error logging when parsing fails

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 const ReadyResource = require('ready-resource')
 const net = require('net')
-const Log = require('bare-logger')
 
-const log = new Log()
+const log = console // new Log()
 
 module.exports = class RLStatsAPI extends ReadyResource {
   constructor(port = 49123, host = '127.0.0.1') {
@@ -14,7 +13,27 @@ module.exports = class RLStatsAPI extends ReadyResource {
     const socket = net.createConnection(port, host)
     this.socket = socket
     this.socket
-      .on('error', (err) => this.emit('error', err))
+      .on('data', (data) => {
+	const jsonString = data.toString()
+        try {
+	  const eventObj = JSON.parse(jsonString)
+	  const { Event, Data } = eventObj
+	  this.emit(Event, Data)
+	} catch (err) {
+	  log.error('connection:data json parsing jsonString', jsonString)
+	  log.error('connection:data json parsing error', err)
+	  if (err.message.match(/JSON at position \d/)) {
+            const positionM = err.message.match(/JSON at position (\d+)/)
+	    console.log('positionM', positionM)
+	    const position = Number(positionM[1])
+            const char = jsonString[position]
+	    console.log('char', char, 'char(hex)', Buffer.from(char))
+	    const bleed = 10
+	    console.log('char slice', jsonString.slice(Math.max(0, position - bleed), Math.min(jsonString.length, position + bleed)))
+	  }
+	}
+      })
+      .on('error', (err) => this.emit('connection:error', err))
       .on('open', () => {
         log.info('socket opened', 'port', port, 'host', host)
         this.emit('connected')
