cdc610e2ef4e58046b357e5d5d780f07bea04055
Sean Zellmer <sean@lejeunerenard.com>
2026-09-06 17:35:09 -0500

    refact: Define decoding partial json as a Channel

--- src/layers/events.ts
+++ src/layers/events.ts
@@ -7,27 +7,56 @@ import { AllEvents } from '../schema/events.js'
 type AllEventsType = typeof AllEvents.Type
 
 export interface RLStatsLive {
-  readonly parsed: Channel.Channel<Chunk.Chunk<Either.Either<AllEventsType, ParseResult.ParseError>>, Chunk.Chunk<string | Uint8Array | Socket.CloseEvent>, SocketError, SocketError>
+  readonly parsed: Channel.Channel<
+    Chunk.Chunk<Either.Either<AllEventsType, ParseResult.ParseError>>,
+    Chunk.Chunk<string | Uint8Array | Socket.CloseEvent>,
+    SocketError,
+    SocketError
+  >
   readonly connected: Effect.Effect<void, SocketError, never>
 }
 
 export class RLStatsService extends Context.Tag('@rlstats/Events')<RLStatsService, RLStatsLive>() {}
+
+const decodeString = <IE, Done>() => {
+  let _buffer = ''
+  const loop: Channel.Channel<
+    Chunk.Chunk<Either.Either<AllEventsType, ParseResult.ParseError>>,
+    Chunk.Chunk<string>,
+    IE,
+    IE,
+    Done,
+    Done,
+    never
+  > = Channel.readWithCause({
+    onInput: (input) =>
+      Channel.zipRight(
+        Channel.write(
+          Chunk.flatMap(input, (str) => {
+            _buffer += str
+            const { results, remainder } = decodeAndParse(_buffer)
+            _buffer = remainder
+            return Chunk.fromIterable(results)
+          })
+        ),
+        loop
+      ),
+    onFailure: Channel.failCause,
+    onDone: Channel.succeed
+  })
+  return loop
+}
 
 export const RLStatsServiceLive = Layer.effect(
   RLStatsService,
   Effect.gen(function* () {
     const connection = yield* ConnectionService
 
-    let _buffer = ''
+    const readChannel = Socket.toChannelMap<SocketError, string>(connection.socket, (data) =>
+      data.toString()
+    )
 
-    const readChannel = Socket.toChannelMap<SocketError, string>(connection.socket, (data) => data.toString())
-
-    const parsed = Channel.mapOut(readChannel, (chunk) => {
-      _buffer += Chunk.toArray(chunk).join('')
-      const { results, remainder } = decodeAndParse(_buffer)
-      _buffer = remainder
-      return Chunk.fromIterable(results)
-    })
+    const parsed = Channel.pipeTo(readChannel, decodeString())
 
     return {
       parsed,
