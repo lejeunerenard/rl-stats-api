cadd170e93b698a84169e365f092025a5c94a290
Sean Zellmer <sean@lejeunerenard.com>
2026-09-12 11:58:50 -0500

    fix: Convert event processing into Sink & pipe w/ parsed as channel
    Sink seems more correct for what i'm trying to do (which is consuming
    data outputs). The piping mechanism seems to be happier type-wise. The
    current type error is that the sink isn't dealing with the error from
    the socket which is fair.

--- src/bin.ts
+++ src/bin.ts
@@ -1,34 +1,12 @@
 import 'bare-encoding/global'
 
 import { header, command, flag } from 'paparam'
-import { Channel, Chunk, Console, Effect, Either, Stream, Layer, ParseResult, Scope } from 'effect'
+import { Channel, Console, Effect, Either, Sink, Layer, ParseResult } from 'effect'
 // import type { SocketError } from '@effect/platform/Socket'
 import { RLStatsService, RLStatsServiceLive } from './layers/events.js'
 import { ConnectionServiceLive } from './layers/connection.js'
 import { RLStatsConfig } from './layers/config.js'
 import type { AllEventsType } from './schema/events.js'
-
-function channelToStream<OutElemInner, InElem, OutErr, InErr, OutDone, InDone, Env>(
-  channel: Channel.Channel<Chunk.Chunk<OutElemInner>, InElem, OutErr, InErr, OutDone, InDone, Env>
-): Stream.Stream<OutElemInner, OutErr, Env> {
-  return Stream.unwrapScoped(
-    Effect.flatMap(Scope.make(), (scope) =>
-      Effect.flatMap(Channel.toPullIn(channel, scope), (pull) => {
-        const loop = (): Stream.Stream<OutElemInner, OutErr, Env> =>
-          Stream.unwrap(
-            Effect.flatMap(pull, (result) => {
-              if (Either.isLeft(result)) {
-                return Stream.fromIterable(Chunk.flatten(result.left)).pipe(Stream.concat(loop()))
-              } else {
-                return Stream.empty
-              }
-            })
-          )
-        return loop()
-      })
-    )
-  )
-}
 
 const cmd = command(
   'rl-stats-api-cli',
@@ -65,7 +43,8 @@ const cmd = command(
         }
       })
 
-    Effect.runFork(Stream.runForEach(channelToStream(service.parsed), parsedToLog))
+    const sinkToLog = Sink.forEach(parsedToLog)
+    Effect.runFork(Channel.run(Channel.pipeTo(service.parsed, Sink.toChannel(sinkToLog))))
   }
 )
 
